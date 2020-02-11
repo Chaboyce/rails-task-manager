89a2055454405e69625d08093c4279fe7b5401e7
Charlotte Margus <charlotte.boyce@hotmail.fr>
2020-02-11 11:34:50 +0100

    add task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_101644) do
+ActiveRecord::Schema.define(version: 2020_02_11_103233) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "details"
-    t.string "text"
-    t.string "completed"
-    t.string "boolean"
+    t.text "details"
+    t.boolean "ompleted"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
